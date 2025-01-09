530d9c53674086df6cbdb967b3cad4c6cca66ed4
Shivamcs536 <2211cs010536@mallareddyuniversity.ac.in>
2025-01-09 19:55:07 +0530

    training

---
+++ looping.js
@@ -0,0 +1,59 @@
+for(let i=1; i<=10;i++){
+    console.log("Javascript");
+}
+
+//1 is odd
+//2is even
+for(let i=1;i<=100;i++){
+    if(i%2 !=0){
+        console.log(i,"odd");
+
+    }else{
+        console.log(i,"even");
+
+    }
+    
+}
+
+//WAJP to find factorial of number 
+// 5=120 ;
+
+// let a=prompt("Enter a number: ")
+// let s=1;
+// for(a;a>=1;a--){
+//     s=s*a;
+// }
+// console.log(s);
+
+mul=1;
+let num=Number(prompt("enter a number"));
+for(let i=1;i<=num;i--){
+    mul=mul*i;
+}
+console.log(mul);
+
+
+//WAJP to find factor of number 
+//12 ,-2 ,4,3,6
+
+
+//while loop
+
+
+// let i=1;
+// while(i<=5){
+//     console.log("Hello wake up and please interact with me  ");
+//     i++;
+// }
+
+
+
+//do while loop
+
+{
+    i++;
+    console.log("hello javascript");
+}while(i>=5);
+
+
+
---
+++ variables.js
@@ -0,0 +1,16 @@
+//globaal scope
+console.log("start")
+
+var a=10;
+console.log(a);
+
+//local scope
+
+let b=20;
+console.log(b);
+
+const c=30;
+console.log(c);
+
+console.log("end")
+
